298811c3d844d73f5abf14e766d6c57023559fc7
somename <someemail>
2017-05-12 12:05:37 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -5,10 +5,11 @@ import org.junit.Test;
 public class TestBowling {
 
 	@Test
-	public void test() {
+	public void test_isStrike() {
+		Frame fr = new Frame(10,0);
+		boolean provera = fr.isStrike();
 		
-		
-		
+		assertEquals("Greska",true,provera);			
 	}
 
 }
